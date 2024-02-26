e541927207bcdc32f41175990f36710103415d82
pulkit-20 <pulkit2469.be21@chitkara.edu.in>
2024-02-26 20:36:36 +0530

    bug resolved

--- src/main/java/com/example/studentapplication/Student.java
+++ src/main/java/com/example/studentapplication/Student.java
@@ -1,4 +1,4 @@
-package main.java.com.example.studentapplication;
+package com.example.studentapplication;
 
 import lombok.Data;
 
--- src/main/java/com/example/studentapplication/StudentService.java
+++ src/main/java/com/example/studentapplication/StudentService.java
@@ -1,4 +1,4 @@
-package main.java.com.example.studentapplication;
+package com.example.studentapplication;
 
 import java.util.List;
 
