b7bddad2fbc46332dcb511284820953b5f7308a3
pulkit-20 <pulkit2469.be21@chitkara.edu.in>
2024-02-26 19:54:56 +0530

    student class

---
+++ src/main/java/com/example/studentapplication/Student.java
@@ -0,0 +1,12 @@
+package main.java.com.example.studentapplication;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private Long id;
+    private String name;
+    private int age;
+    private String adhar;
+    private String university;
+}
---
+++ src/main/java/com/example/studentapplication/StudentService.java
@@ -0,0 +1,9 @@
+package main.java.com.example.studentapplication;
+
+import java.util.List;
+
+public interface StudentService {
+    Long addStudent(Student student);
+    List<Student> getAllStudentsByUniversity(String university);
+    Student getStudentById(Long id);
+}
